5d313b71ac9a576fc87c733fd83e5c34f654d641
Zihang Wen <wenzihang0506@gmail.com>
2025-08-06 09:20:32 -0400

    entil ppo with graph

--- ENTIL/arguments.py
+++ ENTIL/arguments.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import Optional
 
 def get_args():
     parser = argparse.ArgumentParser(description='RL')
@@ -6,72 +7,121 @@ def get_args():
         '--env-name',
         type=str,
         default="HalfCheetah-v5",
-        help='environment to train on (default: HalfCheetah-v5)')
+        help='environment to train on (default: HalfCheetah-v5)'
+    )
     parser.add_argument(
         '--num-envs',
         type=int,
         default=4,
-        help='number of parallel environments (default: 16)')
+        help='number of parallel environments (default: 16)'
+    )
     parser.add_argument(
         '--entropy-coef',
         type=float,
         default=0,
-        help='entropy term coefficient (default: 0)')
+        help='entropy term coefficient (default: 0)'
+    )
     parser.add_argument(
         '--out-dir',
         type=str,
         default="results",
-        help='directory to save results (default: results)')
+        help='directory to save results (default: results)'
+    )
     parser.add_argument(
         '--T-max',
         type=int,
         default=1000,
-        help='number of steps per episode (default: 1000)')
+        help='number of steps per episode (default: 1000)'
+    )
     parser.add_argument(
         '--n-epochs',
         type=int,
         default=1000,
-        help='number of epochs to run (default: 1000)')
+        help='number of epochs to run (default: 1000)'
+    )
     parser.add_argument(
         '--gamma',
         type=float,
         default=0.99,
-        help='discount factor (default: 0.99)')
+        help='discount factor (default: 0.99)'
+    )
     parser.add_argument(
         '--epsilon',
         type=float,
         default=0.2,
-        help='clip ratio for epsilon-greedy action selection (default: 0.2)')
+        help='clip ratio for epsilon-greedy action selection (default: 0.2)'
+    )
     parser.add_argument(
         '--lam',
         type=float,
         default=0.97,
-        help='Lambda for GAE-Lambda. (Always between 0 and 1, close to 1.)')
+        help='lambda for GAE-Lambda. (Always between 0 and 1, close to 1.)'
+    )
     parser.add_argument(
         '--target-kl',
         type=float,
         default=0.01,
-        help='Roughly what KL divergence we think is appropriate between new and old policies after an update. This will get used for early stopping. (Usually small, 0.01 or 0.05.)')
+        help='roughly what KL divergence we think is appropriate between new and old policies after an update. This will get used for early stopping. (Usually small, 0.01 or 0.05.)'
+    )
     parser.add_argument(
         '--train-a-iters',
         type=int,
         default=80,
-        help='Maximum number of gradient descent steps to take on policy loss per epoch. (Early stopping may cause optimizer to take fewer than this.)')
+        help='maximum number of gradient descent steps to take on policy loss per epoch. (Early stopping may cause optimizer to take fewer than this.)'
+    )
     parser.add_argument(
         '--train-v-iters',
         type=int,
         default=80,
-        help='Number of gradient descent steps to take on value function per epoch.')
+        help='number of gradient descent steps to take on value function per epoch.'
+    )
     parser.add_argument(
         '--n-hidden',
         type=int,
         default=64,
-        help='number of hidden units in the actor and critic networks (default: 64)')
+        help='number of hidden units in the actor and critic networks (default: 64)'
+    )
     parser.add_argument(
         '--seed',
         type=int,
         default=0,
         help='random seed (default: 0)')
+    parser.add_argument(
+        '--graph-file',
+        type=str,
+        default="MAES/graphs/wm.txt",
+        help='file containing the population strucuture (default: MAES/graphs/wm.txt)'
+    )
+    parser.add_argument(
+        '--sol-pool-size',
+        type=int,
+        default=5,
+        help='size of the solution pool (default: 5)'
+    )
+    parser.add_argument(
+        '--max-clones',
+        type=int,
+        default=5,
+        help='maximum number of clones in solution pool <= sol-pool-size (default: 5)'
+    )
+    parser.add_argument(
+        '--select-period',
+        type=int,
+        default=10,
+        help='number of epochs between two selection phases (default: 10)'
+    )
+    parser.add_argument(
+        '--eval-period',
+        type=Optional[int],
+        default=None,
+        help='number of epochs between two evaluation phases (default: None, no evaluation)'
+    )
+    parser.add_argument(
+        '--n-gens',
+        type=int,
+        default=10,
+        help='number of generations of evolutionary algorithm (default: 10)'
+    )
 
     args = parser.parse_args()
 
--- ENTIL/test_entil_local.py
+++ ENTIL/test_entil_local.py
@@ -1,9 +1,10 @@
 import sys
 import time
 import os
+from pathlib import Path
 
 NCORE = "1"
-MAX_RUNTIME = 2
+MAX_RUNTIME = 1
 
 os.environ["OMP_NUM_THREADS"] = NCORE
 os.environ["OPENBLAS_NUM_THREADS"] = NCORE
@@ -11,39 +12,64 @@ os.environ["MKL_NUM_THREADS"] = NCORE
 os.environ["VECLIB_MAXIMUM_THREADS"] = NCORE
 os.environ["NUMEXPR_NUM_THREADS"] = NCORE
 
+import pickle
 import numpy as np
-
 import torch 
-import torch.nn as nn
-import torch.optim as optim
+# import torch.nn as nn
+# import torch.optim as optim
 #import torch.multiprocessing as mp
 import multiprocessing as mp
 
-from previous.world import *
-from previous.util import *
-from previous.wrapper import *
-from previous.model import *
+import gymnasium as gym
 
-def make_checkpoint(sol_pool):
-    checkpoint = dict()
-    for i, sol in enumerate(sol_pool):
-        checkpoint["sol%d" % i] = sol.agent.state_dict()
-        checkpoint["opt%d" % i] = sol.optimizer.state_dict()
-        checkpoint["norm_a%d" % i] = sol.norm_advantage
-        checkpoint["norm_r%d" % i] = sol.norm_reward
-    return checkpoint
+# from algo.wrapper import *
+from algo.agent import AgentConfig, AgentModule
+from algo.ppo import PPO
+from arguments import get_args
+
+# from previous.world import *
+# from previous.util import *
+from algo.wrapper import (
+    load_G,
+    Pop_wrapper,
+    Population,
+)
+# from previous.model import *
+
+# def make_checkpoint(sol_pool):
+#     checkpoint = dict()
+#     for i, sol in enumerate(sol_pool):
+#         checkpoint["sol%d" % i] = sol.agent.state_dict()
+#         checkpoint["opt%d" % i] = sol.optimizer.state_dict()
+#         checkpoint["norm_a%d" % i] = sol.norm_advantage
+#         checkpoint["norm_r%d" % i] = sol.norm_reward
+#     return checkpoint
 
 # PPO
 if __name__ == '__main__':
 ############################################################################################
-    graph_file = sys.argv[1]
-    n_episodes = int(sys.argv[2])
-    n_games = int(sys.argv[3])
-    c_entropy = float(sys.argv[4])
-    internalize = (sys.argv[5] == "True")
-    out_file = sys.argv[6]
+    game_name = "HalfCheetah-v5"
+    n_games = 4
+    T_max = 1000
+    n_epochs = 250
+    seed = 0
+    out_dir = Path(f"data/ENTIL_graph/{game_name}")
 
-    # graph_file = "MAES/graphs/wm.txt"
+    gamma = 0.99
+    epsilon = 0.2
+    lam = 0.97
+    target_kl = 0.01
+    train_a_iters = 80
+    train_v_iters = 80
+    n_hidden = 64
+
+    # ----- ----- ----- graph params ----- ----- ----- #
+    graph_file = "MAES/graphs/wm.txt"
+    sol_pool_size = 5
+    max_clones = 5
+    select_period = 10
+    n_gens = 10
+
     # n_episodes = 30000
     # n_games = 1024
     # c_entropy = 0.5
@@ -52,130 +78,162 @@ if __name__ == '__main__':
 
     num_of_process = 5
 
-    print("# cpus : ", os.cpu_count() )
-    print("# torch interloop threads : ", torch.get_num_interop_threads() )
-    print("# torch intraloop threads : ", torch.get_num_threads() )
-    print("# internalize: %s, graph file: %s, entropy: %g" % (internalize, graph_file, c_entropy))
+    # print("# cpus : ", os.cpu_count() )
+    # print("# torch interloop threads : ", torch.get_num_interop_threads() )
+    # print("# torch intraloop threads : ", torch.get_num_threads() )
+    # print("# internalize: %s, graph file: %s, entropy: %g" % (internalize, graph_file, c_entropy))
     
 ############################################################################################
-    game_config = {'T_max' : 100,
-                   'N_games' : n_games,
-                   'N_agents' : 2,
-                   'N_landmarks' : 3}
+    game_config = {
+        'T_max' : T_max,
+        'N_games' : n_games,
+        'game_name' : game_name,
+    }
 
-    training_config = {'gamma' : 0.95,
-                       'std_u' : 0.1,
-                       'epsilon' : 0.1,
-                       'c_entropy': c_entropy}
+    training_config = {
+        'gamma' : gamma,
+        'epsilon' : epsilon,
+        # 'entropy_coef': entropy_coef,
+        'lam' : lam,
+        'target_kl' : target_kl,
+        'train_a_iters' : train_a_iters,
+        'train_v_iters' : train_v_iters,
+    }
 
 ############################################################################################
-    sol_pool_size = 5
     sol_pool = []
     for sol_idx in range(sol_pool_size):
-        scenario = Scenario()
-        world = scenario.make_world(n_games, game_config['N_agents'], game_config['N_landmarks'], internalize)
-        env = SimpleEnv(world, scenario)
+        # scenario = Scenario()
+        # world = scenario.make_world(n_games, game_config['N_agents'], game_config['N_landmarks'], internalize)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
 
-        sol = Solution()
-        sol.env = env
+        envs = gym.make_vec(
+            game_config['game_name'], num_envs=game_config['N_games'], vectorization_mode="sync"
+        )
+        envs.reset(seed=seed)
 
-        sol.training_config = training_config
-        sol.game_config = game_config
+        n_obs = envs.observation_space.shape[1]
+        n_action = envs.action_space.shape[1]
+        action_type = envs.action_space.__class__.__name__
+        n_hidden = 64
+
+        cfg = AgentConfig(
+            n_input_actor = n_obs,
+            n_input_critic = n_obs,
+            n_action = n_action,
+            action_type = action_type,
+            n_hidden = n_hidden,
+        )
+
+        agent = AgentModule(cfg)
+
+        algo = PPO(envs, agent)
+        algo.training_config = training_config
+        algo.game_config = game_config
         
-        sol.idx = sol_idx
-        sol_pool += [sol]
+        algo.idx = sol_idx
+        sol_pool += [algo]
 
-    hyp_pool = [0, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1]
-    max_clones = 5
-
-    select_period, eval_period = 100, 10
+############################################################################################
+    # hyp_pool = [0, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1]
+    hyp_pool = [None]
     starting_epoch = 0
     data = []
     t_start = time.time()
     runtime = 0
     
-############################################################################################
-    try:
-        print("temp_dir/%s.checkpoint"% out_file.split("/")[-1])
-        checkpoint = torch.load("temp_dir/%s.checkpoint"% out_file.split("/")[-1])
-        for i_sol, sol in enumerate(sol_pool):
-            sol.agent.load_state_dict(checkpoint["sol%d" % i_sol])
-            sol.optimizer.load_state_dict(checkpoint["opt%d" % i_sol])
-            sol.norm_advantage = checkpoint["norm_a%d" % i_sol]
-            sol.norm_reward = checkpoint["norm_r%d" % i_sol]
+    # try:
+    #     print("temp_dir/%s.checkpoint"% out_file.split("/")[-1])
+    #     checkpoint = torch.load("temp_dir/%s.checkpoint"% out_file.split("/")[-1])
+    #     for i_sol, sol in enumerate(sol_pool):
+    #         sol.agent.load_state_dict(checkpoint["sol%d" % i_sol])
+    #         sol.optimizer.load_state_dict(checkpoint["opt%d" % i_sol])
+    #         sol.norm_advantage = checkpoint["norm_a%d" % i_sol]
+    #         sol.norm_reward = checkpoint["norm_r%d" % i_sol]
             
-        pop = checkpoint["pop_wrapper"]
-        starting_epoch = checkpoint['epoch']
-        data = checkpoint['data']
-        runtime = checkpoint['runtime']
-        print("# Checkpoint loaded.")
-    except:
-        print("# No checkpoint detected.")
-        G = load_G(graph_file)
-        pop = Pop_wrapper(Population(G), sol_pool_size, len(hyp_pool), max_clones)
+    #     pop = checkpoint["pop_wrapper"]
+    #     starting_epoch = checkpoint['epoch']
+    #     data = checkpoint['data']
+    #     runtime = checkpoint['runtime']
+    #     print("# Checkpoint loaded.")
+    # except:
+    #     print("# No checkpoint detected.")
+    #     G = load_G(graph_file)
+    #     pop = Pop_wrapper(Population(G), sol_pool_size, len(hyp_pool), max_clones)
+    G = load_G(graph_file)
+    pop = Pop_wrapper(Population(G), sol_pool_size, len(hyp_pool), max_clones)
 
 ############################################################################################
     print("# Starting freq: ", np.array(pop.freq_current))
-    worker_pool = mp.Pool(processes = num_of_process)
-    for i_epoch in range(starting_epoch, n_episodes, select_period):
+    # worker_pool = mp.Pool(processes = num_of_process)
+    for i_epoch in range(starting_epoch, n_epochs, select_period):
         # Checkpoint
-        if (time.time() - t_start) // 3600 >= MAX_RUNTIME:
-            runtime += (time.time() - t_start)
-            print("# Checkpoint saving at: ", (time.time() - t_start) / 3600)
-            print("# Current freq: ", np.array(pop.freq_current))
-            checkpoint = make_checkpoint(sol_pool)
-            checkpoint["pop_wrapper"] = pop
-            checkpoint['epoch'] = i_epoch
-            checkpoint['data'] = data
-            checkpoint['runtime'] = runtime
-            torch.save(checkpoint, "temp_dir/%s.checkpoint" % out_file.split("/")[-1])
+        # if (time.time() - t_start) // 3600 >= MAX_RUNTIME:
+        #     runtime += (time.time() - t_start)
+        #     print("# Checkpoint saving at: ", (time.time() - t_start) / 3600)
+        #     print("# Current freq: ", np.array(pop.freq_current))
+        #     checkpoint = make_checkpoint(sol_pool)
+        #     checkpoint["pop_wrapper"] = pop
+        #     checkpoint['epoch'] = i_epoch
+        #     checkpoint['data'] = data
+        #     checkpoint['runtime'] = runtime
+        #     torch.save(checkpoint, "temp_dir/%s.checkpoint" % out_file.split("/")[-1])
             
-            worker_pool.close()
-            worker_pool.join()
-            sys.exit(85)
+        #     worker_pool.close()
+        #     worker_pool.join()
+        #     sys.exit(85)
         
         # Training and evaluation
-        procs = []
-        for sol_idx, hyp_idx in pop.sol_current:
-            sol = sol_pool[sol_idx]
-            sol.hyper_config = {'std_c' : hyp_pool[hyp_idx]}
-            procs += [worker_pool.apply_async(sol.wrapper, (select_period, eval_period))]
+        # procs = []
+        # for sol_idx, hyp_idx in pop.sol_current:
+        #     sol = sol_pool[sol_idx]
+        #     sol.hyper_config = {'std_c' : hyp_pool[hyp_idx]}
+        #     procs += [worker_pool.apply_async(sol.wrapper, (select_period, eval_period))]
             
+        # results = []
+        # for proc in procs:
+        #     temp = proc.get()
+
         results = []
-        for proc in procs:
-            temp = proc.get()
-            results += [temp[0]]
-            sol = sol_pool[temp[1]]
-            sol.optimizer.load_state_dict(temp[2])
-            sol.norm_advantage = temp[3]
-            sol.norm_reward = temp[4]
+        for sol_idx, hyp_idx in pop.sol_current:
+            algo = sol_pool[sol_idx]
+            logger = algo.train(select_period)
+
+            # sol.hyper_config = {'std_c' : hyp_pool[hyp_idx]}
+
+            # temp = sol.wrapper(select_period, eval_period)
+            logger.get_logs()
+            temp = logger._logs["AverageEpRet"]  # list, len == select_period
+            results += [temp]
+            # sol = sol_pool[temp[1]]
+            # sol.optimizer.load_state_dict(temp[2])
+            # sol.norm_advantage = temp[3]
+            # sol.norm_reward = temp[4]
             
         # Logging
-        results_train = np.stack([r[0] for r in results]).T
-        results_eval = np.stack([r[1] for r in results]).T
+        # results_train = np.stack([r[0] for r in results]).T
+        # results_eval = np.stack([r[1] for r in results]).T
+        results = np.array(results).T
 
         freq = np.array(pop.freq_current)
-        fitness = results_train[-1]
+        fitness = results[-1]
 
-        for i, r in enumerate(results_eval):
-            data += [(i_epoch + eval_period * i, (r * freq).sum(), r.max())]
-            print(*data[-1])
+        for i, r in enumerate(results):
+            d_temp = [(i_epoch + select_period * i, (r * freq).sum(), r.max())]
+            data += d_temp
+            print(*d_temp)
         
         # Evolution
         if i_epoch > 0:
-            pop.update(fitness)
+            pop.update(fitness, n_gens)
             
-    worker_pool.close()
-    worker_pool.join()
+    # worker_pool.close()
+    # worker_pool.join()
 
     print("# runtime: ", runtime + time.time() - t_start)
-    np.savetxt(out_file + ".txt", data, fmt='%g')
-    np.savetxt(out_file + "_c.txt", pop.visit_cnts, fmt='%g')
-    np.savetxt(out_file + "_f.txt", pop.visit_fits, fmt='%g')
-
-    checkpoint = make_checkpoint(sol_pool)
-    checkpoint["pop_wrapper"] = pop
-    checkpoint['epoch'] = i_epoch
-    checkpoint['data'] = data
-    checkpoint['runtime'] = runtime
-    torch.save(checkpoint, out_file + "_final.checkpoint")
+    # np.savetxt(out_file + ".txt", data, fmt='%g')
+    # np.savetxt(out_file + "_c.txt", pop.visit_cnts, fmt='%g')
+    # np.savetxt(out_file + "_f.txt", pop.visit_fits, fmt='%g')
+    with open(out_dir / "data.pkl", 'wb') as f:
+        pickle.dump(data, f)
